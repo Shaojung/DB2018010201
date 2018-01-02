d0f5bc494432f3e1d5410ab169c012f428162567
Shaojung <shaojung@gmail.com>
2018-01-02 19:06:07 +0800

    DAO

--- app/src/main/java/com/example/student/db2018010201/MainActivity.java
+++ app/src/main/java/com/example/student/db2018010201/MainActivity.java
@@ -6,17 +6,23 @@ import android.util.Log;
 
 import com.example.student.db2018010201.data.Student;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    ArrayList<Student> mylist = new ArrayList();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Student s = new Student();
-        s.id = 1;
-        s.name = "Bob";
-        s.score = 95;
-        Log.d("Data", s.id + "," + s.name + "," + s.score);
-
+        Student s1 = new Student(1, "Bob", 95);
+        Student s2 = new Student(2, "Mary", 90);
+        Student s3 = new Student(3, "Peter", 98);
+        mylist.add(s1);
+        mylist.add(s2);
+        mylist.add(s3);
+        for (Student t : mylist)
+        {
+            Log.d("DATA", t.toString());
+        }
     }
 }
--- app/src/main/java/com/example/student/db2018010201/data/Student.java
+++ app/src/main/java/com/example/student/db2018010201/data/Student.java
@@ -8,4 +8,14 @@ public class Student {
     public int id;
     public String name;
     public int score;
+    public Student(int id, String name, int score)
+    {
+        this.id = id;
+        this.name = name;
+        this.score = score;
+    }
+    public String toString()
+    {
+        return id + "," + name + "," + score;
+    }
 }
